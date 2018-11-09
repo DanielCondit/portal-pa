daa18688e4db768e08ca6255612fa8ed5ef34160
Daniel Condit <DanielJCondit@gmail.com>
2018-11-09 11:49:33 -0600

    Trying to change routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,16 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Routes, Router, RouterModule, ActivatedRoute, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
+  private router: Router;
   title = 'portal-pa';
 // My adds
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
@@ -21,6 +24,9 @@ export class AppComponent {
     );
 
   items: Observable<any[]>;
-  constructor(private breakpointObserver: BreakpointObserver, db: AngularFirestore) {this.items = db.collection('items').valueChanges();
+  constructor(private breakpointObserver: BreakpointObserver, db: AngularFirestore, router: Router) {
+    this.items = db.collection('items').valueChanges();
+    this.router = router;
   }
 }
+
