b7af73107aee92a2127a0dee0a8aab12093dff0d
DanielCondit <DanielJCondit@gmail.com>
2018-11-10 02:40:18 +0000

    production fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,27 @@
-// native imports
+//native imports 
 import { NgModule } from '@angular/core';
-import { Routes, Router, RouterModule, ActivatedRoute, RouterOutlet} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-// my imports
+//my imports
 import { CommonModule } from '@angular/common';
-import { PaHomeComponent } from './pa-home/pa-home.component';
-import { Form833Component } from './form833/form833.component';
-import { EquipFormComponent } from './equip-form/equip-form.component';
-import { Component, OnInit } from 'node_modules/@angular/core';
-import { AppComponent } from './app.component';
+import { PaHomeComponent }      from './pa-home/pa-home.component';
+import { Form833Component }      from './form833/form833.component';
+import { EquipFormComponent }      from './equip-form/equip-form.component';
 
-export const routes: Routes = [
-// my routes
- { path: 'PA_Home', component: PaHomeComponent },
- { path: 'Form_833', component: Form833Component },
- { path: 'Equip_Form', component: EquipFormComponent }
+const routes: Routes = [
+	//my routes
+  // { path: '', redirectTo: '/PA_Portal', pathMatch: 'full' },
+  { path: 'PA_Portal', component: PaHomeComponent },
+	{ path: 'Form_833', component: Form833Component },
+	{ path: 'Equip_Form', component: EquipFormComponent }
 ];
 
 @NgModule({
   imports: [
-   RouterModule.forRoot(routes),
-   CommonModule
-  ],
+  	RouterModule.forRoot(routes),
+  	CommonModule
+  	],
   declarations: [],
   exports: [RouterModule]
 })
-
-export class AppRoutingModule {}
+export class AppRoutingModule { }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,25 @@
-// Native imports
+//Native imports
 import { Component } from '@angular/core';
 
-// My Imports
+//My Imports
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { Routes, Router, RouterModule, ActivatedRoute, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-
 export class AppComponent {
-  private router: Router;
   title = 'portal-pa';
-// My adds
+  //My adds
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
 
   items: Observable<any[]>;
-  constructor(private breakpointObserver: BreakpointObserver, db: AngularFirestore, router: Router) {
-    this.items = db.collection('items').valueChanges();
-    this.router = router;
-  }
+  constructor(private breakpointObserver: BreakpointObserver, db: AngularFirestore) {this.items = db.collection('items').valueChanges();};
 }
-
